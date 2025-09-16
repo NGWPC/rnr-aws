5422f9e9ec40334da7737f75db60674d23d10015
taddyb <tkb5476@psu.edu>
2025-09-16 09:28:58 -0400

    patch: removed bad import

--- lambdas/producer/src/hml_reader/__init__.py
+++ lambdas/producer/src/hml_reader/__init__.py
@@ -1,6 +1,5 @@
 from hml_reader.client import async_get, get
 from hml_reader.schemas.weather import HML
-from hml_reader.publish import fetch_data
 from hml_reader.settings import Settings
 
-__all__ = ["async_get", "get", "fetch_data", "HML", "Settings"]
+__all__ = ["async_get", "get", "HML", "Settings"]

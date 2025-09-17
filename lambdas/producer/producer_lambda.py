17a39dc8b88f443eca86c19080f1b90e7ba56e98
kevin.spurrier <kevin.spurrier@rtx.com>
2025-09-17 16:00:00 -0500

    Producer Lambda: default vhost to /

--- lambdas/producer/producer_lambda.py
+++ lambdas/producer/producer_lambda.py
@@ -107,12 +107,13 @@ def lambda_handler(event, context):
     creds = pika.PlainCredentials(user, pwd)
     url = urlparse(rabbit_mq_endpoint)
     context = ssl.create_default_context()
+    vhost = url.path.strip('/') if url.path.strip('/') else '/'
 
     try:
         conn = pika.BlockingConnection(pika.ConnectionParameters(
             host=url.hostname,
             port=url.port,
-            virtual_host=url.path,
+            virtual_host=vhost,
             credentials=creds,
             ssl_options=pika.SSLOptions(context),
             heartbeat=30,
